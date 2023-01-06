ffb5217cb8a7d6e62d38a13e01e10bc337166115
udstuenj <udstuenj@gmail.com>
2023-01-06 10:20:22 -0500

    changed email

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
       <main>
         <Header title="WELCOME TO JERRY UDSTUEN'S WEBSITE FOR CONNECTION, EXPRESSION, AND ANYTHING ELSE"/>
         <p className="description">
-          i love correspondance <br /> feel free to contact me <br /> judstn@gmail.com <br /> on-request, i can add you to my personal newsletter <br /> <br /> <br /> <br /> works <br /> <br />
+          i love correspondance <br /> feel free to contact me <br /> judstn@proton.me <br /> on-request, i can add you to my personal newsletter <br /> <br /> <br /> <br /> works <br /> <br />
           <Link href="https://drive.google.com/file/d/1VxPUIh3FtiB5li4Um2ZzQ3vUCQ7GSH3f/view?usp=sharing">
           Aklas saga
           </Link> <br />
