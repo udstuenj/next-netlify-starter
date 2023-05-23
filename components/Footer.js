e4e0d69d94142fba49830810415cf943ccdf3fa9
udstuenj <udstuenj@gmail.com>
2023-05-23 16:50:52 -0400

    whoops linebreak in wrong place

--- components/Footer.js
+++ components/Footer.js
@@ -6,9 +6,9 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         updated 5/23/2023 <br />
-        <Link href="https://www.yourworldoftext.com/~judstn/guestbook"> <br />
+        <Link href="https://www.yourworldoftext.com/~judstn/guestbook">
         guestbook
-        </Link> 
+        </Link> <br />
       </footer>
     </>
   )
