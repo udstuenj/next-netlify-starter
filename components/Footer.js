79cbd23c2cb53321829d71248fa2d74cc3e99a45
udstuenj <udstuenj@gmail.com>
2023-05-23 18:05:40 -0400

    hopefully deleted an extra space

--- components/Footer.js
+++ components/Footer.js
@@ -5,8 +5,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        site updated 5/23/2023 -
-        &nbsp;
+        site updated 5/23/2023 -&nbsp;
         <br /> <Link href="https://www.yourworldoftext.com/~judstn/guestbook">
         guestbook
         </Link>
