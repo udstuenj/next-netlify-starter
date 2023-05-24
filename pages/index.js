c4b42fea691a79cb3b3bf8497a513b0e5d244043
udstuenj <udstuenj@gmail.com>
2023-05-24 16:50:18 -0400

    hehe added line break

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
           </Link> <br />
           <Link href="https://drive.google.com/drive/folders/19KEU8ffqrFqJz0I-Gfcu4H5d2iyXHCSg?usp=sharing">
           Blacksmithing
-          </Link>
+          </Link> <br />
           <Link href="https://drive.google.com/drive/folders/1nBTqXa__gaAiWKksh33-SgwQrhXpbQDL?usp=sharing">
           TTRPG Archive
           </Link>
